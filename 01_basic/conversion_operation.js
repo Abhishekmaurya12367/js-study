58f30bc7bcf4a4388fa4c2ba66984189f473cc03
Abhishek Maurya <166213264+Abhishekmaurya12367@users.noreply.github.com>
2025-05-20 07:33:04 +0000

    sumary datatye

---
+++ 01_basic/conversion_operation.js
@@ -0,0 +1,19 @@
+let score=33
+console.log(typeof(score))
+let score1="33"
+console.log(typeof(score1))
+let number=Number(score1)
+console.log(number)
+console.log(typeof(number))
+//33=>33
+// "33"=>33
+// "33abd"=>NaN
+// "true"=>1
+// "false"=>0
+let logged=1
+let logged1=Boolean(logged)
+console.log(logged1)
+let result=98
+let result1=String(result)
+console.log(typeof(result1))
+
